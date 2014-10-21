cbc49e4f6ec01cff3194354c15f5030b0d16ebc3
julTitova <titova_u_m@mail.ru>
2014-10-21 22:51:11 +0700

    modified 21.10.14 Signed-off-by: julTitova <titova_u_m@mail.ru>

--- Main_test.py
+++ Main_test.py
@@ -4,22 +4,29 @@ import unittest
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
+    def test_Tree(self):
         b = Leo('Tree')
         self.assertEqual('Tree', b.Object_view, "This is incorrect result")
         Leo.Hungry(b)
         Leo.Full(b)
+
+    def test_EmptyString(self):
         c = Leo('')
         self.assertEqual('', c.Object_view, "This is incorrect result")
         Leo.Hungry(c)
         Leo.Full(c)
+
+    def test_Number(self):
         d = Leo(10)
         self.assertEqual(10, d.Object_view, "This is incorrect result")
         Leo.Hungry(d)
         Leo.Full(d)
+
+    def test_Letter(self):
         e = Leo('k')
         self.assertEqual('k', e.Object_view, "This is incorrect result")
         Leo.Hungry(e)
         Leo.Full(e)
+
 if __name__ == '__main__':
     unittest.main()
